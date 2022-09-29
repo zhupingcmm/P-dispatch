a7b2acde4be972bc8e67e420118ab8754908ebcd
pingzhu <ping.zhu@microfocus.com>
2022-09-29 21:10:43 +0800

    add readme doc

--- server/src/main/java/com/mf/server/dispatch/Dispatch.java
+++ server/src/main/java/com/mf/server/dispatch/Dispatch.java
@@ -1,9 +1,13 @@
 package com.mf.server.dispatch;
 
-import com.mf.dispatch.common.base.ProbeInfo;
+
 import com.mf.dispatch.common.base.Task;
 
 public interface Dispatch <T extends Task>{
 
+    /**
+     * 把task 派发给 probe
+     * @param task
+     */
     void dispatch(T task);
 }
